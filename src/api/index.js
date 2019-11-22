d6571c80c9008045dfa3ec6237497299babd0ab1
Pujit Mehrotra <pujitm2009@gmail.com>
2019-11-21 20:38:29 -0500

    Add dialogflow-fulfillment

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import logger from 'config/logger'
+import { WebhookClient } from 'dialogflow-fulfillment';
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,9 +10,12 @@ app.use(express.json())
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/hook', (req, res) => {
+  const agent = new WebhookClient({req, res});
   logger.info('Request Body', req.body);
   logger.info('Request Params', req.params);
+  logger.info('Request Headers', req.headers);
+  agent.add('Aight, we on it.');
   res.sendStatus(201);
-})
+});
 
 app.listen(port, () => logger.info(`Listening on port ${port}!`))
