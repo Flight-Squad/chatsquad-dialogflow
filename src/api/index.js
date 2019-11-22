0f4067088d0a9577ce4e15651c2a9fdfc8cc6897
Pujit Mehrotra <pujitm2009@gmail.com>
2019-11-21 20:22:08 -0500

    Add POST /hook

--- src/api/index.js
+++ src/api/index.js
@@ -8,4 +8,10 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/hook', (req, res) => {
+  logger.info('Request Body', req.body);
+  logger.info('Request Params', req.params);
+  res.sendStatus(201);
+})
+
 app.listen(port, () => logger.info(`Listening on port ${port}!`))
