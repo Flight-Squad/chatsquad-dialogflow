64d153b422f82e39397aac62c8dffd8c41c197f5
Pujit Mehrotra <pujitm2009@gmail.com>
2019-11-22 12:55:54 -0500

    Attempt intent linking

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import logger from 'config/logger'
 import { WebhookClient } from 'dialogflow-fulfillment';
 import {sessionClient} from 'config/gcp';
 import { projectId } from '../config/gcp';
+import Axios from 'axios';
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -18,10 +19,19 @@ app.post('/hook', async function (req, res) {
   // logger.info('Request Params', req.params);
   // logger.info('Request Headers', req.headers);
   const agent = new WebhookClient({request: req, response: res});
+  const sesssionId = await parseSessionId(agent.session);
   logger.info(`Session ${JSON.stringify(agent.session)}`);
 
   let intentMap = new Map();
-  intentMap.set('flight.book', () => agent.add('Aight, we on it.'));
+  intentMap.set('flight.book', async () => {
+    agent.add('Aight, we on it.');
+    Axios.post('https://chatsquad-webhook.herokuapp.com/sendPrices', {
+      sessionId: agent.session,
+      data: {
+        yolo: 'hi',
+      },
+    })
+  });
   intentMap.set('flight.show', () => agent.add('Will show prices'));
   // intentMap.set('Default Fallback Intent', fallback);
   // intentMap.set('your intent name here', yourFunctionHandler);
@@ -30,11 +40,17 @@ app.post('/hook', async function (req, res) {
   // res.sendStatus(201);
 });
 
+async function parseSessionId(sessionPath) {
+  const parts = sessionPath.split('/');
+  return parts[parts.length - 1];
+}
+
 app.post('/sendPrices', (req, res) => {
   const {sessionId, ...data} = req.body;
   sessionClient.detectIntent(
     {
-      session: `projects/${projectId}/agent/sessions/${sessionId}`,
+      // session: `projects/${projectId}/agent/sessions/${sessionId}`,
+      session: sessionId,
 
       // Trigger a response event
       // https://googleapis.dev/nodejs/dialogflow/latest/google.cloud.dialogflow.v2beta1.html#.QueryInput
