d4487bef87b87a7863c5299b3b9e3f781c425ea6
Pujit Mehrotra <pujitm2009@gmail.com>
2019-11-29 14:47:47 -0500

    Add missing await in onFlightShow

--- src/api/hook/flight/show.ts
+++ src/api/hook/flight/show.ts
@@ -1,10 +1,12 @@
 import Axios from "axios";
 import { parseSessionId } from "actions/parse/session/id";
+import logger from "config/logger";
 
 export async function onFlightShow(agent) {
   // retrieve data based on session id
   const baseUri = process.env.PRICESQUAD_API;
-  const docId = `${parseSessionId(agent.session)}#${agent.context.get('request-id').parameters.id}`;
+  const docId = `${await parseSessionId(agent.session)}#${agent.context.get('request-id').parameters.id}`;
+  logger.info('onFlightShoq docId', { docId });
   const pricesquadReq = await Axios.get(`${baseUri}/prices/${docId}`);
   const priceData = pricesquadReq.data.res;
   const bestGPrices = priceData.google.data.map(quote => quote.price).sort();
