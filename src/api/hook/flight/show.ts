e56b267aa03034afafd9fa6e626d4ba7ab3cc84c
Pujit Mehrotra <pujitm2009@gmail.com>
2019-11-29 15:30:56 -0500

    Remove logging from onFlightShow

--- src/api/hook/flight/show.ts
+++ src/api/hook/flight/show.ts
@@ -3,13 +3,15 @@ import { parseSessionId } from "actions/parse/session/id";
 import logger from "config/logger";
 
 export async function onFlightShow(agent) {
-  // retrieve data based on session id
   const baseUri = process.env.PRICESQUAD_API;
+
+  // retrieve data based on session id
   const sessionId = await parseSessionId(agent.session);
   const requestId = await agent.context.get('request-id').parameters.id;
+
   const req = await Axios.get(`${baseUri}/prices/${sessionId}/${requestId}`);
   const priceData = req.data.res;
-  console.log(priceData);
   const bestGPrices = priceData.google.data.map(quote => quote.price).sort();
+
   agent.add(`Parameters: ${JSON.stringify(bestGPrices)}`);
 }
