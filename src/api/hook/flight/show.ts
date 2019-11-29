e7e401bb87b7d90d731c77110449603a0da21ee5
Pujit Mehrotra <pujitm2009@gmail.com>
2019-11-29 14:52:48 -0500

    Debug priceData object in onFlightShow

--- src/api/hook/flight/show.ts
+++ src/api/hook/flight/show.ts
@@ -6,9 +6,9 @@ export async function onFlightShow(agent) {
   // retrieve data based on session id
   const baseUri = process.env.PRICESQUAD_API;
   const docId = `${await parseSessionId(agent.session)}#${agent.context.get('request-id').parameters.id}`;
-  logger.info('onFlightShoq docId', { docId });
   const pricesquadReq = await Axios.get(`${baseUri}/prices/${docId}`);
-  const priceData = pricesquadReq.data.res;
+  const priceData = pricesquadReq.data;
+  console.log(priceData);
   const bestGPrices = priceData.google.data.map(quote => quote.price).sort();
   agent.add(`Parameters: ${JSON.stringify(bestGPrices)}`);
 }
