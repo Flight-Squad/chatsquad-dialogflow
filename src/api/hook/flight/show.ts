24b3d1b373309d5256975b8ee6bcf3411c00d1fb
Pujit Mehrotra <pujitm2009@gmail.com>
2019-12-08 23:03:27 +0000

    Add trip obj integration in onFLightShow

--- src/api/hook/flight/show.ts
+++ src/api/hook/flight/show.ts
@@ -12,12 +12,28 @@ export async function onFlightShow(agent) {
 
   // TODO Change this when pricesquad GET is updated
   const req = await Axios.get(`${baseUri}/prices/${docPath}`);
-  const priceData = req.data.prices;
+  const priceData = req.data.res;
 
   if (priceData) {
-    agent.add(`The best price online is $${priceData[0]}!`);
+    const bestTrip = priceData[0];
+    const {airline, stops, duration, times, price, layover} = bestTrip;
+    const ourPrice = calculateTemplatePrice(price);
+    const sepTimes = times.split('-');
+    const takeoff = sepTimes[0].trim();
+    const arrival = sepTimes[1].trim();
+    agent.add(`It looks like the public price for this trip is around $${price} right now.`);
+    agent.add(`So far, we found a $${ourPrice} ${stops} ${duration} trip leaving at ${takeoff} and landing at ${arrival} local time operated by ${airline}.`);
+    agent.add('Our human agents will follow up soon to further assist you!');
   } else {
     // TODO error reporting and debugging info
     agent.add("Something went wrong. I'm sorry! A human team member will be in touch shortly.");
   }
+}
+
+function calculateTemplatePrice(price: number) {
+  if (price <= 300) return (price * 0.94).toFixed(2);
+  if (price > 300 && price <= 500) return (price * 0.89).toFixed(2);
+
+  // else
+  return (price * 0.86).toFixed(2);
 }
