d63a7815edd5601decd9a73a464f844b46a7d809
Pujit Mehrotra <pujitm2009@gmail.com>
2019-12-09 22:47:17 +0000

    Debug payload entry and request headers

--- src/api/hook/index.ts
+++ src/api/hook/index.ts
@@ -13,7 +13,8 @@ hookRouter.post('/hook', async (request, response) => {
   // logger.info('Request Params', req.params);
   // logger.info('Request Headers', req.headers);
   const agent = new WebhookClient({request, response});
-  console.log(agent.originalRequest);
+  console.log(agent.originalRequest.payload.body.entry);
+  console.log(request.headers);
   // logger.info('Orig request', request.body.originalDetectIntentRequest);
   // logger.info('Agent Contexts', agent.contexts);
   // console.log(agent.context.get('request-id'));
