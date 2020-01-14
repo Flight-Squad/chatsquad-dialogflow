cfeb264fd442455adf3c97cd91d4be3919ba4719
Pujit Mehrotra <pmehrotra2@babson.edu>
2020-01-14 00:48:03 -0500

    Add FIREBASE_URL

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 Database.init({
   serviceAccount: process.env.FIRESTORE_CONFIG,
+  firebaseUrl: process.env.FIREBASE_URL,
   serviceAccountPath: path.resolve(__dirname, "./serviceAccount.json")
 });
 
