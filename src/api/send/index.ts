f0584a9e6cb9a4d6e53ca7ce84891043daba3e0a
Pujit Mehrotra <pujitm2009@gmail.com>
2020-01-26 19:49:20 -0500

    Update messaging routes

--- src/api/send/index.ts
+++ src/api/send/index.ts
@@ -3,11 +3,9 @@ import { projectId } from "config/gcp";
 import logger from "config/logger";
 import { sendPricesToDialogflow } from "actions/send/prices/toDialogflow";
 import sendMessagesRouter from "./messages";
-import sendPricesRouter from "./prices";
 
 const sendRouter = Router();
 
 sendRouter.use('/messages', sendMessagesRouter);
-sendRouter.use('/prices', sendPricesRouter);
 
 export default sendRouter;
--- src/api/send/messages/index.ts
+++ src/api/send/messages/index.ts
@@ -1,16 +1,46 @@
 import { Router } from "express";
+import { makePriceMessage } from "actions/make/priceMessage";
+import { sendMessage } from "actions/send/messages";
+import { PaymentMessageReq } from "@flight-squad/common";
 
 const sendMessagesRouter = Router();
 
-sendMessagesRouter.post('/messenger', async (req, res) => {
+sendMessagesRouter.post("/", async (req, res) => {
+  const { id, platform, message } = req.body;
+  await sendMessage(platform, id, message);
+  res.sendStatus(201);
+});
+
+/**
+ * Body:
+ *
+    isRoundTrip: Boolean,
+    platform: string (e.g 'facebook')
+    userId: string,
+    trips: Array<any>,
+ */
+sendMessagesRouter.post("/payment", async (req, res) => {
+  const reqData: PaymentMessageReq = req.body;
+  const message = await makePriceMessage(
+    reqData.platform,
+    reqData.query.isRoundTrip,
+    reqData.trip,
+    reqData.payment.url
+  );
+  await sendMessage(reqData.platform, reqData.id, message);
+
+  res.sendStatus(201);
+});
+
+sendMessagesRouter.post("/messenger", async (req, res) => {
   // TODO implement
 });
 
-sendMessagesRouter.post('/slack', async (req, res) => {
+sendMessagesRouter.post("/slack", async (req, res) => {
   // TODO implement
 });
 
-sendMessagesRouter.post('/wechat', async (req, res) => {
+sendMessagesRouter.post("/wechat", async (req, res) => {
   // TODO implement
 });
 
