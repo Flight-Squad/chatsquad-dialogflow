995c0f71bd8aaf8f16113d8ab2b3ceb4583cb4ad
Pujit Mehrotra <pujitm2009@gmail.com>
2020-01-28 18:20:14 -0500

    Return message on success

--- src/actions/make/priceMessage.ts
+++ src/actions/make/priceMessage.ts
@@ -4,9 +4,8 @@ import { Trip } from "@flight-squad/admin";
 export async function makePriceMessage(platform: string, isRoundTrip: boolean, trip: Trip, paymentUrl: string) {
   logger.info('Trips', {trip: JSON.stringify(trip, null, 2)});
   const stops = trip.stops.length - 2 <= 0 ? 'non-stop' : `${trip.stops.length - 2}-stop`
-  let message: string;
   try {
-    message = `So far, we found a $${trip.price} trip! You can find more details at ${paymentUrl}.
+    return `So far, we found a $${trip.price} trip! You can find more details at ${paymentUrl}.
 
 ** If anything seems off, please let us know, and our human agents will be with you shortly!`;
   } catch(e) {
